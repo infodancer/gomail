81a707aef16ed98565d2b26e9e06af515c827bdb
Matthew Hunter <matthew@infodancer.org>
2019-03-15 02:36:23 -0500

    Add unique name function

--- maildir/maildir.go
+++ maildir/maildir.go
@@ -1,5 +1,28 @@
 package maildir
 
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
+
+var deliveryCounter int64
+
+// CreateMaildir creates a maildir directory structure
 func CreateMaildir(name string) {
 
+}
+
+func createUniqueName() string {
+	date := time.Now()
+	left := date.Nanosecond()
+	center := rand.Int63()
+	right, err := os.Hostname()
+	if err != nil {
+
+	}
+
+	result := fmt.Sprintf("%v.%v.%v", left, center, right)
+	return result
 }
