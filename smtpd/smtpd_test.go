bad7e624eb7bd76629684e4be204ea16b67d6b68
Matthew Hunter <matthew@infodancer.org>
2023-07-03 17:20:59 -0500

    fix smtpd tests

--- smtpd/smtpd_test.go
+++ smtpd/smtpd_test.go
@@ -1,8 +1,10 @@
 package smtpd
 
 import (
-	"strings"
 	"testing"
+
+	"github.com/infodancer/gomail/connect"
+	"github.com/stretchr/testify/assert"
 )
 
 // TestextractAddress extracts an address from a RCPT TO or MAIL FROM line
@@ -35,27 +37,43 @@ func TestIsSuspiciousAddress(t *testing.T) {
 }
 
 func TestHandleInputLine(t *testing.T) {
-	s := &Session{}
-
-	if code, result, finished := s.HandleInputLine("HELO hi"); code != 250 || !strings.HasSuffix(result, "Hello") || finished {
-		t.Error("Invalid response to HELO: ", result)
-	}
-	if code, result, finished := s.HandleInputLine("EHLO hi"); code != 250 || !strings.HasSuffix(result, "Hello") || finished {
-		t.Error("Invalid response to EHLO: ", result)
-	}
-	if code, result, finished := s.HandleInputLine("NOOP"); code != 250 || !strings.HasPrefix(result, "OK") || finished {
-		t.Error("Invalid response to NOOP: ", result)
-	}
-	if code, result, finished := s.HandleInputLine("RSET"); code != 250 || !strings.HasPrefix(result, "OK") || finished {
-		t.Error("Invalid response to RSET: ", result)
-	}
-	if code, result, finished := s.HandleInputLine("VRFY"); code != 500 || !strings.HasPrefix(result, "VRFY not supported") || finished {
-		t.Error("Invalid response to VRFY: ", result)
-	}
-	if code, result, finished := s.HandleInputLine("MAIL FROM:<test@example.com>"); code != 250 || !strings.HasPrefix(result, "OK") || finished {
-		t.Error("Invalid response to MAIL FROM: ", code, result, finished)
-	}
-	if code, result, finished := s.HandleInputLine("QUIT"); code != 221 || strings.HasPrefix(result, " goodbye") || !finished {
-		t.Error("Invalid response to QUIT: ", result)
-	}
+	cfg := Config{ServerName: "testserver"}
+	c, err := connect.NewStandardIOConnection()
+	assert.NoError(t, err, "unable to create connection")
+	s := &Session{Conn: c, Config: cfg}
+	success := 250
+	endsession := 221
+	failure := 500
+	code, result, finished := s.HandleInputLine("HELO hi")
+	assert.Equal(t, success, code, "result code was not 250")
+	assert.Contains(t, result, "testserver")
+	assert.False(t, finished)
+	code, result, finished = s.HandleInputLine("EHLO hi")
+	assert.Equal(t, success, code, "result code was not 250")
+	assert.Contains(t, result, "testserver")
+	assert.False(t, finished)
+	code, result, finished = s.HandleInputLine("NOOP")
+	assert.Equal(t, success, code, "result code was not 250")
+	assert.Contains(t, result, "OK")
+	assert.False(t, finished)
+	code, result, finished = s.HandleInputLine("RSET")
+	assert.Equal(t, success, code, "result code was not 250")
+	assert.Contains(t, result, "OK")
+	assert.False(t, finished)
+	code, result, finished = s.HandleInputLine("VRFY")
+	assert.Equal(t, failure, code, "VRFY command did not return 500")
+	assert.Contains(t, result, "VRFY not supported")
+	assert.False(t, finished)
+	code, result, finished = s.HandleInputLine("VRFY test@example.com")
+	assert.Equal(t, failure, code, "VRFY command did not return 500")
+	assert.Contains(t, result, "VRFY not supported")
+	assert.False(t, finished)
+	code, result, finished = s.HandleInputLine("MAIL FROM:<test@example.com>")
+	assert.Equal(t, success, code, "MAIL FROM command did not return 250")
+	assert.Contains(t, result, "OK")
+	assert.False(t, finished)
+	code, result, finished = s.HandleInputLine("QUIT")
+	assert.Equal(t, endsession, code, "QUIT command did not return 221")
+	assert.Contains(t, result, "goodbye")
+	assert.True(t, finished)
 }
