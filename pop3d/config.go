a8fc744df43867a938f823a724a3d0f9b2a09c7d
Matthew <56937361+infodancer@users.noreply.github.com>
2023-07-04 08:13:29 -0500

    Add debian pkg (#1)
    * remove deprecated call
    
    * Add configuration support
    
    * Add debian package info
    
    * Add package script
    
    * Add gitignore for build dir

--- pop3d/config.go
+++ pop3d/config.go
@@ -1,11 +1,29 @@
 package pop3d
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+
 	"github.com/infodancer/gomail/connect"
 )
 
 type Config struct {
-	ServerName string
+	ServerName string `json:"servername"`
+}
+
+func ReadConfigFile(file string) (*Config, error) {
+	c := Config{}
+	data, err := os.ReadFile(file)
+	if err != nil {
+		return nil, fmt.Errorf("error reading pop3d config from %s: %w", file, err)
+	}
+
+	err = json.Unmarshal(data, &c)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling pop3d config from %s: %w", file, err)
+	}
+	return &c, nil
 }
 
 // Start sends the banner for new connections
